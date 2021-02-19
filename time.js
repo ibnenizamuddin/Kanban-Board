4e087d59ebc6c9bac06a7f574f42f87262a1c849
Ehsanullah Shaikh <ibnenizamuddin@gmail.com>
2021-02-19 06:38:01 -0500

    Style change and removed the
    Made the clock look betetr

--- time.js
+++ time.js
@@ -1,8 +1,6 @@
 const timediv = document.getElementById('time-list');
-//const mainBody= document.getElementsByTagName("BODY")[0];
-let PhotoImage=[];
-function setWordTime() {
 
+function setWordTime() {
     let d = new Date();
     let currentHour = d.getHours();
     let currentMinute = d.getMinutes();
@@ -21,32 +19,20 @@ function setWordTime() {
     else if (currentMinute === 30) {
         setCurrentTime = `It's <span class="main">${timeArray[currentMinute]}</span> past <span class="main">${HourArray[currentHour]}</span> `;
     }
-    else if (timeArray[60 - currentMinute] === 45) {
+    else if (currentMinute === 45) {
         setCurrentTime = `It's <span class="main">${timeArray[60 - currentMinute]}</span> to <span class="main">${HourArray[currentHour + 1]}</span>  `;
     }
     else {
         setCurrentTime = `It's <span class="main">${timeArray[60 - currentMinute]}</span> minute to <span class="main">${HourArray[currentHour + 1]}</span>  `;
     }
+
     timediv.innerHTML = setCurrentTime;
+  
 }
+
 setWordTime();
-setInterval(setWordTime, 60 * 1000);
-
-setInterval(setBackgrounPhoto, 600 * 1000);
+setInterval(setWordTime, 5 * 1000);
 
 
-async function setBackgrounPhoto(){
-    try{
-            //get API Key
-    const APIKey='';
-    const URL=`https://api.unsplash.com/photos/random?count=1&client_id=${APIKey}`;
-    let response = await fetch(URL);
-    PhotoImage= await response.json();
-    document.body.style.backgroundImage = `url('${PhotoImage[0].urls.regular}')`;
 
-    }
-    catch (error){
-        //Error
-    }
 
-}
